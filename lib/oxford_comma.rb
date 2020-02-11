ed39f3919c5298929db7f6c8cb73e2bab2696e61
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-10 19:16:24 -0800

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_word = array.pop
+    array << "and #{last_word}"
+    array.join(", ")
+  end
 end
