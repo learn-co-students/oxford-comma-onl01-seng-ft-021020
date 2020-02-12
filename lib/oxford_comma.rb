fdf19641da7bf059289118a2bc9447fc42887394
Kyle Talmage <ktalmage85@gmail.com>
2020-02-12 19:26:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+ 
+ if array.count == 3
+   return array.first(2).join(", ") + ", and " + array.last
+  elsif array.count > 3
+  return array[0..-2].join(", ") + ", and " + array[-1]
+  else
+  return array.join(" and ")
+  end   
 end
