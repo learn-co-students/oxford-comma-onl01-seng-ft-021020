d9d4b085c72965caf5eb393d2f0ba63449c47238
Syed zaidi <jzaidi@live.com>
2020-02-20 08:28:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+=begin def oxford_comma(array)
+=end end
+   
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
